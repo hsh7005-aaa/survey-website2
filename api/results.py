3fc93a19ef6a2df82ba72ff07dadc35dbd96eb18
hsh7005-aaa <hsh7005@gmail.com>
2026-05-21 20:42:55 +0900

    Update results.py

--- api/results.py
+++ api/results.py
@@ -2,24 +2,28 @@ import json
 import os
 from collections import Counter
 from http.server import BaseHTTPRequestHandler
-from urllib.parse import urlparse
 
 import gspread
-from google.oauth2.service_account import Credentials
+from google.oauth2.credentials import Credentials as OAuthCredentials
 
 from auth import get_admin_from_cookie
 
-SCOPES = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive"
-]
 
-
-def get_sheet():
-    creds_dict = json.loads(os.environ["GOOGLE_SERVICE_ACCOUNT_JSON"])
-    creds = Credentials.from_service_account_info(creds_dict, scopes=SCOPES)
+def get_sheet_with_token(access_token):
+    creds = OAuthCredentials(token=access_token)
     client = gspread.authorize(creds)
     return client.open_by_key(os.environ["SPREADSHEET_ID"]).sheet1
+
+
+def get_access_token_from_cookie(headers):
+    cookie_header = headers.get("Cookie", "")
+    cookies = {}
+    for part in cookie_header.split(";"):
+        part = part.strip()
+        if "=" in part:
+            k, v = part.split("=", 1)
+            cookies[k.strip()] = v.strip()
+    return cookies.get("access_token", "")
 
 
 class handler(BaseHTTPRequestHandler):
@@ -30,7 +34,6 @@ class handler(BaseHTTPRequestHandler):
         self.end_headers()
 
     def do_GET(self):
-        # 관리자 인증 확인
         admin = get_admin_from_cookie(self.headers)
         if not admin:
             self.send_response(401)
@@ -41,7 +44,8 @@ class handler(BaseHTTPRequestHandler):
             return
 
         try:
-            sheet = get_sheet()
+            access_token = get_access_token_from_cookie(self.headers)
+            sheet = get_sheet_with_token(access_token)
             all_rows = sheet.get_all_records()
 
             result = {
